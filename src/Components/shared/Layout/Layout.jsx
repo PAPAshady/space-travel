00c906d519773b7428f3bfa3cfe412ac8a3ef997
Nima Zamani <zamani.nima18@gmail.com>
2025-01-06 15:32:18 +0330

    feat(Layout.jsx, Home.jsx, Home.css, index.css, tailwind.config.js): added Header html and css template.

--- src/Components/shared/Layout/Layout.jsx
+++ src/Components/shared/Layout/Layout.jsx
@@ -1,4 +1,59 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link, NavLink } from 'react-router-dom';
+import HamburgerMenuIcon from '../../../assets/images/shared/icon-hamburger.svg';
+import Logo from '../../../assets/images/shared/logo.svg';
+import PropTypes from 'prop-types';
+
 export default function Layout() {
-  return <Outlet />;
+  const navLinks = [
+    { id: 1, title: 'HOME', href: '/' },
+    { id: 2, title: 'DESTINATION', href: '/destination' },
+    { id: 3, title: 'CREW', href: '/crew' },
+    { id: 4, title: 'THECHNOLOGY', href: '/thechnology' },
+  ];
+
+  return (
+    <div className="relative">
+      <header className="fixed w-full">
+        <div>
+          <div className="relative flex items-center justify-between lg:container lg:mt-8">
+            <Link to="/" className="p-4 md:px-8 lg:me-12 lg:px-0">
+              <img src={Logo} alt="Logo" />
+            </Link>
+            <span className="z-20 hidden h-[1px] flex-1 translate-x-8 bg-white/50 lg:block"></span>
+            <div className="md:flex-1 lg:flex-grow-0">
+              <div className="hidden items-center justify-end gap-8 bg-blue-dark px-12 tracking-wider text-white backdrop-blur-md md:flex lg:gap-12 lg:bg-[rgba(255,255,255,0.1)] lg:ps-52 xl:ps-64">
+                {navLinks.map((link) => (
+                  <HeaderNavLink key={link.id} {...link} />
+                ))}
+              </div>
+              <button className="p-4 md:hidden">
+                <img src={HamburgerMenuIcon} alt="Menu" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
 }
+
+function HeaderNavLink({ title, href }) {
+  return (
+    <NavLink
+      className={({ isActive }) =>
+        `border-b-4 pb-6 pt-8 font-bellefair text-sm transition-colors duration-300 hover:border-white/50 xl:py-8 ${isActive ? 'border-white' : 'border-transparent'}`
+      }
+      to={href}
+    >
+      {title}
+    </NavLink>
+  );
+}
+
+HeaderNavLink.propTypes = {
+  title: PropTypes.string.isRequired,
+  href: PropTypes.string.isRequired,
+};
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,12 @@
+import './Home.css';
+
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div
+      id="home-wrapper"
+      className="h-[100dvh] bg-cover bg-bottom bg-no-repeat pt-[120px] text-white sm:bg-center lg:pt-[160px]"
+    >
+      <div className="container"></div>
+    </div>
+  );
 }
